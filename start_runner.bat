a495a6fe9f5fabca76d5bd12c3682c060d2fab15
Roman <noagard@gmail.com>
2025-10-04 00:18:01 +0400

    Init

---
+++ start_runner.bat
@@ -0,0 +1,17 @@
+@echo off
+REM 1. Запускаем Docker-демон в WSL
+wsl -d Ubuntu-22.04 -u root -e bash -c "service docker start"
+
+REM 2. Ждём пока сокет /var/run/docker.sock появится
+echo Waiting for Docker to start...
+:waitloop
+wsl -d Ubuntu-22.04 -u root -e bash -c "test -S /var/run/docker.sock"
+if errorlevel 1 (
+    timeout /t 2 >nul
+    goto waitloop
+)
+
+REM 3. Запускаем раннер под пользователем ld58
+wsl -d Ubuntu-22.04 -u template --cd /home/ld58/actions-runner ./run.sh
+
+pause
